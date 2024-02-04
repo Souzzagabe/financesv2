aea41c8fa0344a86f3e625a1c53cb04f91bf14f7
Gabriel <souzzagabe@hotmail.com>
2024-02-04 15:23:58 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as C from './App.Styles';
+
+const  App = () => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Finances</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        content
+      </C.Body>
+    </C.Container>
+  )
+}
+
+export default App;
